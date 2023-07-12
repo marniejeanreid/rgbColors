7b4260e6fb0379fba617e4ce1ad2b3716d7444a8
Marnie Reid <marnie@kodewithklossy.com>
2023-07-12 13:57:08 -0500

    Initial Commit

---
+++ rgbColors/rgbColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rgbColorsApp.swift
+//  rgbColors
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct rgbColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
